6fa70f90eefb038c165cb7f6de9a1194cbfc2d54
Balazs Racz <balazs.racz@gmail.com>
2018-12-17 22:01:55 +0100

    Adds suites to run all connection config tests.

---
+++ java/test/jmri/ConnectionConfigXmlRepRepTest.java
@@ -0,0 +1,24 @@
+package jmri;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author Randall Wood (C) 2016
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+      ConnectionConfigXmlRepTest.class,      
+      ConnectionConfigXmlRepTest.class,      
+      ConnectionConfigXmlRepTest.class,      
+      ConnectionConfigXmlRepTest.class,      
+      ConnectionConfigXmlRepTest.class,      
+      ConnectionConfigXmlRepTest.class,      
+      ConnectionConfigXmlRepTest.class,      
+      ConnectionConfigXmlRepTest.class,      
+      ConnectionConfigXmlRepTest.class,      
+      ConnectionConfigXmlRepTest.class,      
+})
+public class ConnectionConfigXmlRepRepTest {
+}
